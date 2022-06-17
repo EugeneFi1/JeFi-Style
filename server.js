2caaaea0d30f8b98b7cab4e2e801d9b1fbc62089
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2022-06-18 01:08:19 +0300

    fix

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/jefi-style'));
+app.use(express.static(__dirname + '/dist/guide'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/jefi-style/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/guide/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
